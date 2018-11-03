aa8514ec366335a001f0a153f437d22ed6c0dd9e
Anika Bagga <anikabagga@gmail.com>
2018-11-03 15:37:40 -0700

    Initial Commit

---
+++ periodic./ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  periodic.
+//
+//  Created by Anika Bagga on 11/3/18.
+//  Copyright © 2018 Anika Bagga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
